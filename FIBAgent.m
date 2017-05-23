5ff27f1dcec4f13b165577006489291e71de95ce
Shalamandia <Shalamandia@pku.edu.cn>
2017-05-23 16:28:24 +0800

    rewrite FIBAgent

--- FIBAgent.m
+++ FIBAgent.m
@@ -1,83 +1,52 @@
 classdef FIBAgent < handle
-    % EXAMPLEAGENT is an agnet just to show what interfaces should an agent
-    % have. It will randomly choose an action each time. 
-    
     properties
-        % Some Properties of an agent
-        % For example, the GGLLG sequence or the data for every possible
-        % sequence of K length (每种K长度的序列的平均收益之类的)
-        posterior;
-        observation;
         value;
         transition;
-        threshold;
+        safe;
+        probability;
     end
     
     methods
-        function obj = FullyInformedBayesianAgent(gain, loss, transition)
-            % Initialization
-            obj.observation = double(Status(randi(2)));
-            obj.value=[gain, gain, loss, loss]';% 只是针对0.9 0.1的那个矩阵，如果是Figure 4的那种需要修改
+        function obj = FIBAgent(gain, loss, transition)
+            obj.value=[gain, gain, loss, loss]';
             obj.transition = transition;
-            obj.threshold=0;
-            obj.posterior=[0.5 0.5];
+            obj.safe=0;
+            obj.probability=[0.25, 0.25, 0.25, 0.25];
+            % in the first trial the 4 probabilities are equal
+            
+            % 这里我着重说明一下，因为在第一个试次中4个状态概率均等
+            % 因此被试用来判断该怎么决定的 probability 矩阵需要在做第一次决定
+            % 前就给出，但是对当前所处状态的概率矩阵是在第一次决定后才有的
+            % 综上，我将 probability 定义为：被试认为下一次4个状态出现的概率
+            
         end
         
         function action = chooseAction(obj)
-        % this function should choose an action between 1(risky) and 2(safe)
-        % 给出一个action
-%         全选risk，全选save，只看上次的
-%             action = (rand > 0.5) + 1;
-            if min(obj.transition(obj.observation*2,:) * obj.value, obj.transition(obj.observation*2-1,:) * obj.value) > obj.threshold
-                action=1;
+            expectation = obj.probability * obj.value;            
+            if expectation == obj.safe
+                action = randi(2);
             else
-                if min(obj.transition(obj.observation*2,:) * obj.value, obj.transition(obj.observation*2-1,:) * obj.value) < obj.threshold
-                action=2;
+                if expectation < obj.safe
+                    action = 2;
                 else
-                    action=randi(2);
+                    action = 1;
                 end
             end
         end
         
-        function [] = updateAgent(obj, results, transition)
-        % this function should take results (contains every action's result)
-        % of last action and update agent. 
-        % 拿到上一次action的results（两个option的results都有），更新这个agent，例如更新每种K长度的序列的平均收益
-        temp = (results(1) < 0) + 1;% 1 is gain, while 2 is loss
-%         if temp == 1
-%             if obj.posterior(1)>obj.posterior(2)
-%                 obj.observation=1;
-%             else
-%                 obj.observation=2;
-%             end
-%             pGain_1
-%         else
-%             if obj.posterior(3)>obj.posterior(4)
-%                 obj.observation=3;
-%             else
-%                 obj.observation=4;
-%             end
-%         end
-        if obj.observation==1
-            if temp==1
-                pGain_1=obj.posterior*[obj.transition(1,1)/(obj.transition(1,1)+obj.transition(1,2));obj.transition(2,1)/(obj.transition(2,1)+obj.transition(2,2))];
-                obj.posterior=[pGain_1 1-pGain_1];
+        function [] = updateAgent(obj, results)
+            % since at this moment the subject has known the result of this
+            % trial, so he will upgrate his knowledge about the present
+            % status of this trial
+            if results(1) > 0
+                p_Gain1 = obj.probability(1) / (obj.probability(1) + obj.probability(2));
+                obj.probability = [p_Gain1, 1 - p_Gain1, 0, 0];
             else
-                pLoss_1=obj.posterior*[obj.transition(1,3)/(obj.transition(1,3)+obj.transition(1,4));obj.transition(2,3)/(obj.transition(2,3)+obj.transition(2,4))];
-                obj.posterior=[pLoss_1 1-pLoss_1];
+                p_Loss1 = obj.probability(3) / (obj.probability(3) + obj.probability(4));
+                obj.probability = [0, 0, p_Loss1, 1 - p_Loss1];
             end
-        else
-            if temp==1
-                pGain_1=obj.posterior*[obj.transition(3,1)/(obj.transition(3,1)+obj.transition(3,2));obj.transition(4,1)/(obj.transition(4,1)+obj.transition(4,2))];
-                obj.posterior=[pGain_1 1-pGain_1];
-            else
-                pLoss_1=obj.posterior*[obj.transition(3,3)/(obj.transition(3,3)+obj.transition(3,4));obj.transition(4,3)/(obj.transition(4,3)+obj.transition(4,4))];
-                obj.posterior=[pLoss_1 1-pLoss_1];
-            end
+            
+            obj.probability = obj.probability * obj.transition;
         end
-%         pGain_1=obj.posterior(3:4)*[obj.transition(3,1)/(obj.transition(3,1)+obj.transition(3,3));obj.transition(4,1)/(obj.transition(4,1)+obj.transition(4,3))];
-%         pLoss_1=obj.posterior(1:2)*[obj.transition(1,2)/(obj.transition(1,2)+obj.transition(1,4));obj.transition(2,2)/(obj.transition(2,2)+obj.transition(2,4))];
-%         obj.posterior(pGain_1,1-pGain_1,pLoss_1,1-pLoss_1);
-      end
-    
+    end
 end
