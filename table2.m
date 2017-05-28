adafdbc47a66960ef2f194afd65c725cc584d03b
ryuryuc <ryuryuhu@gmail.com>
2017-05-28 22:47:42 +0800

    table1&2
    根据table4改了table12 数看着挺对的
    asv我文件夹里明明没有啊(

--- table2.m
+++ table2.m
@@ -1,148 +1,69 @@
-ProbNum = 1;
-TrialNum = 100; % Trial Number should take 100 only
-for prob = 1:ProbNum
-    %% agent setting
-    FIBA = FIBAgent(Vgain, Vloss, transition);
-    FP = DisAvrAgent(1); FP_OpTrial = 0;
-    DisAvr1 = DisAvrAgent(0); DA1_OpTrial = 0;
-    DisAvr2 = DisAvrAgent(0.25); DA2_OpTrial = 0;
-    DisAvr3 = DisAvrAgent(0.5); DA3_OpTrial = 0;
-    DisAvr4 = DisAvrAgent(0.75); DA4_OpTrial = 0;
-    DisAvr5 = DisAvrAgent(0.9); DA5_OpTrial = 0;
-    CABk1 = CABKAgent(1); CABk1_OpTrial = 0;
-    CABk2 = CABKAgent(2); CABk2_OpTrial = 0;
-    CABk3 = CABKAgent(3); CABk3_OpTrial = 0;
-    CABk4 = CABKAgent(4); CABk4_OpTrial = 0;
-    CABk5 = CABKAgent(5); CABk5_OpTrial = 0;
-    CABk6 = CABKAgent(6); CABk6_OpTrial = 0;
-    FIBA_reward = 0;
-    FP_reward = 0; DisAvr1_reward = 0; DisAvr2_reward = 0;
-    DisAvr3_reward = 0; DisAvr4_reward = 0; DisAvr5_reward = 0;
-    CABk1_reward = 0; CABk2_reward = 0; CABk3_reward = 0;
-    CABk4_reward = 0; CABk5_reward = 0; CABk6_reward = 0;
+clear;
+
+problem_count = 1;
+trial_count = 100;
+
+agent_count = 14;
+reward_record = zeros(problem_count, agent_count);
+optimal_record = zeros(problem_count, agent_count);
+actions_record = zeros(problem_count, agent_count);
+
+for problem = 1:problem_count
+    % set up env
+    gain = 1;
+    loss = -10;
+    state = 2;
+    transition = [0.9, 0.1; 0.9, 0.1];
+    env = TwoOptionsEnv(state, gain, loss, transition);
     
-    %% game environment parameters
-    transition = [0.9,0.1;0.9,0.1];
-    Vgain = 1;
-    Vloss = -10;
-    env = TwoOptionsEnv(Vgain, Vloss, transition);
+    % set up agent
+    FIBA = FIBAgent(gain, loss, transition);
+    FP = DisAvrAgent(1);
+    DisAvr1 = DisAvrAgent(0);
+    DisAvr2 = DisAvrAgent(0.1);
+    DisAvr3 = DisAvrAgent(0.25);
+    DisAvr4 = DisAvrAgent(0.5);
+    DisAvr5 = DisAvrAgent(0.75);
+    DisAvr6 = DisAvrAgent(0.9);
+    CABk1 = CABKAgent(1);
+    CABk2 = CABKAgent(2);
+    CABk3 = CABKAgent(3);
+    CABk4 = CABKAgent(4);
+    CABk5 = CABKAgent(5);
+    CABk6 = CABKAgent(6);
+    agents = {FIBA, FP, DisAvr1, DisAvr2, DisAvr3, DisAvr4, DisAvr5, DisAvr6, CABk1, CABk2, CABk3, CABk4, CABk5, CABk6};
+    assert(agent_count == size(agents, 2));
+    rewards = zeros(1, agent_count);
+    optimal = zeros(1, agent_count);
+    actions = zeros(1, agent_count);
     
-    %% trials for each problem and agents take actions
-    for trial = 1:TrialNum
-        % FIBA
-        FIBA_action = FIBA.chooseAction();
-        [result, whole_result] = env.getResult(FIBA_action);
-        FIBA_reward = FIBA_reward + result;
-        FIBA.updateAgent(whole_result);
-        % FP
-        FP_action = FP.chooseAction();
-        [result, whole_result] = env.getResult(FP_action);
-        FP_reward = FP_reward + result;
-        FP.updateAgent(whole_result);
-        if FP_action == FIBA_action
-            FP_OpTrial = FP_OpTrial+1;
+    for i = 1:trial_count
+        action_FIBA = agents{1}.chooseAction();
+        [result, whole_result] = env.getResult(action_FIBA);
+        agents{1}.updateAgent(whole_result);
+        rewards(1) = rewards(1) + result;
+        optimal(1) = optimal(1) + 1;
+        actions(1) = actions(1) + (action_FIBA == 1);
+        for j = 2:agent_count
+            agent = agents{j};
+            action = agent.chooseAction();
+            [result, whole_result] = env.getResult(action);
+            agent.updateAgent(whole_result);
+            rewards(j) = rewards(j) + result;
+            optimal(j) = optimal(j) + (action == action_FIBA);
+            actions(j) = actions(j) + (action == 1);
         end
-        % DisAvr
-        DisAvr1_action = DisAvr1.chooseAction();
-        [result, whole_result] = env.getResult(DisAvr1_action);
-        DisAvr1_reward = DisAvr1_reward + result;
-        DisAvr1.updateAgent(whole_result);
-        if DisAvr1_action == FIBA_action
-            DA1_OpTrial = DA1_OpTrial+1;
-        end
-        DisAvr2_action = DisAvr2.chooseAction();
-        [result, whole_result] = env.getResult(DisAvr2_action);
-        DisAvr2_reward = DisAvr2_reward + result;
-        DisAvr2.updateAgent(whole_result);
-        if DisAvr2_action == FIBA_action
-            DA2_OpTrial = DA2_OpTrial+1;
-        end
-        DisAvr3_action = DisAvr3.chooseAction();
-        [result, whole_result] = env.getResult(DisAvr3_action);
-        DisAvr3_reward = DisAvr3_reward + result;
-        DisAvr3.updateAgent(whole_result);
-        if DisAvr3_action == FIBA_action
-            DA3_OpTrial = DA3_OpTrial+1;
-        end
-        DisAvr4_action = DisAvr4.chooseAction();
-        [result, whole_result] = env.getResult(DisAvr4_action);
-        DisAvr4_reward = DisAvr4_reward + result;
-        DisAvr4.updateAgent(whole_result);
-        if DisAvr4_action == FIBA_action
-            DA4_OpTrial = DA4_OpTrial+1;
-        end
-        DisAvr5_action = DisAvr5.chooseAction();
-        [result, whole_result] = env.getResult(DisAvr5_action);
-        DisAvr5_reward = DisAvr5_reward + result;
-        DisAvr5.updateAgent(whole_result);
-        if DisAvr5_action == FIBA_action
-            DA5_OpTrial = DA5_OpTrial+1;
-        end
-        % CABk
-        CABk1_action = CABk1.chooseAction();
-        [result, whole_result] = env.getResult(CABk1_action);
-        CABk1_reward = CABk1_reward + result;
-        CABk1.updateAgent(whole_result);
-        if CABk1_action == FIBA_action
-            CABk1_OpTrial = CABk1_OpTrial+1;
-        end
-        CABk2_action = CABk2.chooseAction();
-        [result, whole_result] = env.getResult(CABk2_action);
-        CABk2_reward = CABk2_reward + result;
-        CABk2.updateAgent(whole_result);
-        if CABk2_action == FIBA_action
-            CABk2_OpTrial = CABk2_OpTrial+1;
-        end
-        CABk3_action = CABk3.chooseAction();
-        [result, whole_result] = env.getResult(CABk3_action);
-        CABk3_reward = CABk3_reward + result;
-        CABk3.updateAgent(whole_result);
-        if CABk3_action == FIBA_action
-            CABk3_OpTrial = CABk3_OpTrial+1;
-        end
-        CABk4_action = CABk4.chooseAction();
-        [result, whole_result] = env.getResult(CABk4_action);
-        CABk4_reward = CABk4_reward + result;
-        CABk4.updateAgent(whole_result);
-        if CABk4_action == FIBA_action
-            CABk4_OpTrial = CABk4_OpTrial+1;
-        end
-        CABk5_action = CABk5.chooseAction();
-        [result, whole_result] = env.getResult(CABk5_action);
-        CABk5_reward = CABk5_reward + result;
-        CABk5.updateAgent(whole_result);
-        if CABk5_action == FIBA_action
-            CABk5_OpTrial = CABk5_OpTrial+1;
-        end
-        CABk6_action = CABk6.chooseAction();
-        [result, whole_result] = env.getResult(CABk6_action);
-        CABk6_reward = CABk6_reward + result;
-        CABk6.updateAgent(whole_result);
-        if CABk6_action == FIBA_action
-            CABk6_OpTrial = CABk6_OpTrial+1;
-        end
+        env.transit();
     end
+    rewards  = rewards / trial_count;
+    % disp(rewards);
+    reward_record(problem, :) = rewards;
+    optimal_record(problem, :) = optimal / trial_count;
+    actions_record(problem,:) = actions / trial_count;
+%     disp(problem);
 end
 
-%% result computing
-% optimal trial percent
-FP_OpTrialPer = FP_OpTrial / (ProbNum*TrialNum);
-DisAvr_OpTrialPer = [DA1_OpTrial / (ProbNum*TrialNum),DA2_OpTrial / (ProbNum*TrialNum),...
-    DA3_OpTrial / (ProbNum*TrialNum),DA4_OpTrial / (ProbNum*TrialNum),DA5_OpTrial / (ProbNum*TrialNum)];
-CABk_OpTrialPer = [CABk1_OpTrial / (ProbNum*TrialNum),CABk2_OpTrial / (ProbNum*TrialNum)...
-    CABk3_OpTrial / (ProbNum*TrialNum),CABk4_OpTrial / (ProbNum*TrialNum),...
-    CABk5_OpTrial / (ProbNum*TrialNum),CABk6_OpTrial / (ProbNum*TrialNum)];
-% mean payoff
-FIBA_MPayoff = mean(FIBA_reward);
-FP_MPayoff = mean(FP_reward);
-DisAvr_MPayoff = [mean(DisAvr1_reward),mean(DisAvr2_reward),mean(DisAvr3_reward),...
-    mean(DisAvr4_reward),mean(DisAvr5_reward)];
-CABk_MPayoff = [mean(CABk1_reward),mean(CABk2_reward),mean(CABk3_reward),...
-    mean(CABk4_reward),mean(CABk5_reward),mean(CABk6_reward)];
-fprintf('FP agent optimal trial percent = %f\n',FP_OpTrialPer);
-fprintf('Discouted average agent optimal trial percent = %f\n',DisAvr_OpTrialPer);
-fprintf('CABk agent optimal trial percent = %f\n',CABk_OpTrialPer);
-fprintf('FIBA mean payoff = %f\n',FIBA_MPayoff);
-fprintf('FP mean payoff = %f\n',FP_MPayoff);
-fprintf('Discounted average mean payoff = %f\n',DisAvr_MPayoff);
-fprintf('CABk mean payoff = %f\n',CABk_MPayoff);
+save('table2.mat', 'reward_record', 'optimal_record');
+% mpayoff = mean(reward_record);
+% optrial_per = mean(optimal_record);
+% save('optrial_per', 'mpayoff');
