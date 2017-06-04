803c787ce34773d4375edf9f93097e9e2e8babcd
Shalamandia <Shalamandia@pku.edu.cn>
2017-06-04 18:14:43 +0800

    fix some problems in figure 5

--- figure5_Redraw.m
+++ figure5_Redraw.m
@@ -2,6 +2,10 @@ hold on;
 line_settings = {'-', '-', '-', '-.', '--', '--', '--'};
 pictures = [2 5 6 7 9 10 13];
 for i = 1:7
-    plot(1:5, proportion(:, pictures(i)), line_settings{i},'LineWidth', 3);
+    plot(1:5, proportion(:, pictures(i)), line_settings{i}, 'LineWidth', 3);
 end
-legend('FP', '\alpha = 0.25', '\alpha = 0.5', '\alpha = 0.75','CAB-1','CAB-2','CAB-5');
+xlabel('Variability Level');
+ylabel('Prop. Optimal Trials')
+set(gca,'xtick',1:5);
+set(gca,'ytick',0.5:0.1:1);
+legend('\alpha = 0.25', '\alpha = 0.5', '\alpha = 0.75', 'FP', 'CAB-1', 'CAB-2', 'CAB-5');
