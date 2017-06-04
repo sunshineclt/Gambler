3b400e0b09790f1cd7a07b229c60c3a267597ff2
Zac Chan <sunshinecltzac@gmail.com>
2017-06-04 15:57:07 +0800

    table1_newCABK

---
+++ table1_newCABK.m
@@ -0,0 +1,84 @@
+clear;
+
+problem_count = 10000;
+trial_count = 100;
+
+agent_count = 14;
+reward_record = zeros(problem_count, agent_count);
+optimal_record = zeros(problem_count, agent_count);
+
+for problem = 1:problem_count
+    % set up env    
+    gain = randi([1, 20]);
+    loss = randi([-20, -1]);
+    state = 4;
+    transition = zeros(state);
+    for row = 1:state
+        column = randi(state);
+        unique = ((state - 1) * rand + 1) / state;
+        transition(row, :) = (1 - unique) / (state - 1);
+        transition(row, column) = unique;
+    end
+    env = TwoOptionsEnv(state, gain, loss, transition);
+
+    % set up agent
+    FIBA = FIBAgent(gain, loss, transition);
+    FP = DisAvrAgent(1);
+    DisAvr1 = DisAvrAgent(0);
+    DisAvr2 = DisAvrAgent(0.1);
+    DisAvr3 = DisAvrAgent(0.25);
+    DisAvr4 = DisAvrAgent(0.5);
+    DisAvr5 = DisAvrAgent(0.75);
+    DisAvr6 = DisAvrAgent(0.9);
+    CABk1 = CABKAgent(1);
+    CABk2 = CABKAgent(2);
+    CABk3 = CABKAgent(3);
+    CABk4 = CABKAgent(4);
+    CABk5 = CABKAgent(5);
+    CABk6 = CABKAgent(6);
+    agents = {FIBA, FP, CABk1, CABk2, CABk3, CABk4, CABk5, CABk6, DisAvr1, DisAvr2, DisAvr3, DisAvr4, DisAvr5, DisAvr6};
+    assert(agent_count == size(agents, 2));
+    rewards = zeros(1, agent_count);
+    optimal = zeros(1, agent_count);
+    
+    for i = 1:trial_count
+        action_FIBA = agents{1}.chooseAction();
+        [result, whole_result] = env.getResult(action_FIBA);
+        agents{1}.updateAgent(whole_result);
+        rewards(1) = rewards(1) + result;
+        optimal(1) = optimal(1) + 1;
+        action_FP = agents{2}.chooseAction();
+        [result, whole_result] = env.getResult(action_FP);
+        agents{2}.updateAgent(whole_result);
+        rewards(2) = rewards(2) + result;
+        optimal(2) = optimal(2) + (action == action_FIBA);
+        previous_action = action_FP;
+        for j = 3:8
+            agent = agents{j};
+            action = agent.chooseAction_(previous_action);
+            [result, whole_result] = env.getResult(action);
+            agent.updateAgent(whole_result);
+            rewards(j) = rewards(j) + result;
+            optimal(j) = optimal(j) + (action == action_FIBA);
+            previous_action = action;
+        end
+        for j = 9:agent_count
+            agent = agents{j};
+            action = agent.chooseAction();
+            [result, whole_result] = env.getResult(action);
+            agent.updateAgent(whole_result);
+            rewards(j) = rewards(j) + result;
+            optimal(j) = optimal(j) + (action == action_FIBA);
+        end
+        env.transit();
+    end
+    rewards  = rewards / trial_count;
+    % disp(rewards);
+    reward_record(problem, :) = rewards;
+    optimal_record(problem, :) = optimal / trial_count;
+    disp(problem);
+end
+
+mpayoff = mean(reward_record);
+optrial_per = mean(optimal_record);
+save('table1.mat', 'reward_record', 'optimal_record','optrial_per', 'mpayoff');
